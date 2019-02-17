60924f1bc241fb2e862518aef42be5c40f5e4675
SoyaOnishi <p5h60esu@s.okayama-u.ac.jp>
2019-02-17 11:27:15 +0900

    Add Environment features

--- src/main/scala/plpc/internal/Environment.scala
+++ src/main/scala/plpc/internal/Environment.scala
@@ -1,5 +1,53 @@
 package plpc.internal
 
-class Environment {
+import scala.collection.mutable.Map
 
+sealed abstract class EnvValue
+case class VarValue(v: Value) extends EnvValue
+case class ValValue(v: Value) extends EnvValue
+case class FunValue(v: Value) extends EnvValue
+
+class Environment(parent: Option[Environment]) {
+  val table = Map[String, EnvValue]()
+
+  def regVar(name: String, v: Value): Unit = {
+    table(name) = VarValue(v)
+  }
+
+  def regVal(name: String, v: Value): Unit = {
+    table(name) = ValValue(v)
+  }
+
+  def regFun(name: String, v: Value): Unit = {
+    table(name) = FunValue(v)
+  }
+
+  def lookup(name: String): Option[Value] = {
+    table.get(name) match {
+      case Some(x) => x match {
+        case VarValue(v) => Some(v)
+        case ValValue(v) => Some(v)
+        case FunValue(v) => Some(v)
+      }
+      case None => parent match {
+        case Some(p) => p.lookup(name)
+        case None => None
+      }
+    }
+  }
+
+  def dump(): String = {
+    table.keysIterator.foldLeft("") { (s, k) =>
+      val newLine = table.get(k) match {
+        case Some(ev) => ev match {
+          case VarValue(v) => s"(var) $v\n"
+          case ValValue(v) => s"(val) $v\n"
+          case FunValue(v) => s"(fun) $v\n"
+        }
+        case None => ""
+      }
+
+      s + newLine
+    }
+  }
 }
