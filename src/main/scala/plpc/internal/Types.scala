04877ebed78f7dc240b77d08372120759b6fc92d
SoyaOnishi <p5h60esu@s.okayama-u.ac.jp>
2019-02-20 18:03:19 +0900

    Add getSignature method

--- src/main/scala/plpc/internal/Types.scala
+++ src/main/scala/plpc/internal/Types.scala
@@ -1,11 +1,33 @@
 package plpc.internal
 
-sealed abstract class Type
+sealed abstract class Type {
+  val getSignature: String
+}
+
+case object NoType extends Type {
+  val getSignature = "-"
+}
 
 sealed abstract class PrimitiveType extends Type
 
-case object Unit extends PrimitiveType
-case object Int extends PrimitiveType
-case object String extends PrimitiveType
-case object Double extends PrimitiveType
-case object Boolean extends PrimitiveType
+case object UnitType extends PrimitiveType {
+  override val getSignature = "U"
+}
+case object IntType extends PrimitiveType {
+  override val getSignature = "I"
+}
+case object StringType extends PrimitiveType {
+  override val getSignature = "S"
+}
+
+case object BoolType extends PrimitiveType {
+  override val getSignature = "B"
+}
+
+case class FuncType(t: Type, paramTypes: List[Type]) extends Type {
+  override lazy val getSignature: String = {
+    val params = paramTypes.foldLeft(""){ _ + _.getSignature}
+
+    s"($params)${t.getSignature}"
+  }
+}
